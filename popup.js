e0bb6d8044614fd63ac493755c9d2efe2835e492
Donald Whitely <dswhitely1@gmail.com>
2021-01-14 19:21:48 -0500

    Added Project Options

--- popup.js
+++ popup.js
@@ -1,3 +1,38 @@
+function createRadioButtons({id, name, prefix}) {
+    const inputElement = document.createElement('input');
+    inputElement.classList.add('form-check-input');
+    inputElement.setAttribute('type', 'radio');
+    inputElement.setAttribute('name', 'jira-options');
+    inputElement.setAttribute('id', id);
+    inputElement.setAttribute('value', prefix);
+    const labelElement = document.createElement('label');
+    labelElement.classList.add('form-check-label');
+    labelElement.setAttribute('for', id);
+    labelElement.innerHTML = name;
+    const wrapperDiv = document.createElement('div');
+    wrapperDiv.classList.add('form-check');
+    wrapperDiv.appendChild(inputElement);
+    wrapperDiv.appendChild(labelElement);
+    const parentElement = document.getElementById('jira-options');
+    parentElement.appendChild(wrapperDiv);
+}
+
+const findPrefix = (elements) => {
+    for (const element of elements) {
+        if (element.checked) {
+            return element.value;
+        }
+    }
+}
+
+const findTicketNumber = (elements) => {
+    for (const element of elements) {
+        if (element.type === 'text') {
+            return element.value;
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let baseUrl;
     const baseUrlSpanElement = document.getElementById('jira-url-base');
@@ -8,9 +43,17 @@ document.addEventListener('DOMContentLoaded', () => {
         baseUrlSpanElement.appendChild(baseUrlTextNode);
     })
 
+    chrome.storage.sync.get('prefixOptions', data => {
+        data.prefixOptions.forEach(prefix => createRadioButtons(prefix));
+        const firstInput = document.querySelector('.form-check > .form-check-input');
+        firstInput.checked = true;
+    })
+
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        const ticketUrl = `/browse/SS-${e.target[0].value}`;
+        const prefix = findPrefix(e.target);
+        const ticketNumber = findTicketNumber(e.target);
+        const ticketUrl = `/browse/${prefix}-${ticketNumber}`;
         const url = `${baseUrl}${ticketUrl}`;
         chrome.tabs.create({url: url})
     })
